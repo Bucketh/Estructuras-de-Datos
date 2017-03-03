569b2caaa348bf2d67f4e38a5706f528ee25ea61
Galilea América Loretto Estrada <Bucketh@users.noreply.github.com>
2017-03-02 21:35:43 -0600

    Update Lista.h

--- Lista.h
+++ Lista.h
@@ -29,3 +29,24 @@ Elem cabeza(Lista l){
 int esvacia(Lista l){
     return l == NULL;
 }
+
+Lista InsOrden(Elem e, Lista l){
+    if(esvacia(l)){
+        return cons(e,l);
+    }
+    else    if(EsMenor(e,cabeza(l))){
+                return cons(e,l);
+            }
+            else{
+                return cons(cabeza(l),InsOrden(e,resto(l)));
+            }
+}
+
+Lista Duplica(Lista l){
+    if(esvacia(l)){
+        return l;
+    }
+    else{
+        return cons(cabeza(l)*2, Duplica(resto(l)));
+    }
+}
